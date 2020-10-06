9c4c7e2a5550357e006ffa924c4fd4a6672717bc
csabagyorfi <csaba@madetech.com>
2020-10-06 13:10:06 +0100

    Add first acceptance test
    We want to be able to move the snake infinitely in one direction when
    not other command has given. We also want to show the board with the
    snake on the board.

---
+++ test/acceptance/test_play_game.py
@@ -0,0 +1,43 @@
+from assertpy import assert_that
+from src.domain.snake import Snake
+from src.use_case.move_snake import MoveSnake
+from src.use_case.show_board import ShowBoard
+
+def test_can_move_the_snake_infinitely_across_the_board():
+    board = [5, 5]
+    snake = Snake([[0, 0]], 'E')
+    move_snake = MoveSnake()
+    show_board = ShowBoard()
+
+    assert_that(show_board.execute(board, snake)).is_equal_to([
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', 'S', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', '']
+    ])
+    move_snake.execute(snake)
+    assert_that(show_board.execute(board, snake)).is_equal_to([
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', 'S', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', '']
+    ])
+    move_snake.execute(snake)
+    assert_that(show_board.execute(board, snake)).is_equal_to([
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['S', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', '']
+    ])
+    move_snake.execute(snake)
+    assert_that(show_board.execute(board, snake)).is_equal_to([
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', 'S'],
+      ['', '', '', '', ''],
+      ['', '', '', '', '']
+    ])
+
