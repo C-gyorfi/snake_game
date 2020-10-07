e975bed29072d2a3d633ca025459348901767574
csabagyorfi <csaba@madetech.com>
2020-10-07 11:29:39 +0100

    Refactor move_snake into helper methods
    Next step is to move the body

--- src/use_case/move_snake.py
+++ src/use_case/move_snake.py
@@ -1,27 +1,32 @@
 class MoveSnake:
     @staticmethod
     def execute(board, snake):
+        head_location = MoveSnake.move_head(snake)
+        MoveSnake.wrap_around_borders(board, head_location)
+        snake.current_location[0] = head_location
+
+    @staticmethod
+    def move_head(snake):
         head_location = snake.current_location[0]
-        board_width = board[0]
-        board_height = board[1]
         if snake.current_heading == 'E':
             head_location = [
                 head_location[0] + 1, head_location[1]]
-            if head_location[0] == board_width:
-                head_location[0] = 0
         elif snake.current_heading == 'N':
             head_location = [
                 head_location[0], head_location[1] + 1]
-            if head_location[1] == board_height:
-                head_location[1] = 0
         elif snake.current_heading == 'W':
             head_location = [
                 head_location[0] - 1, head_location[1]]
-            if head_location[0] == - 1:
-                head_location[0] = board_width - 1
         elif snake.current_heading == 'S':
             head_location = [
                 head_location[0], head_location[1] - 1]
-            if head_location[1] == - 1:
-                head_location[1] = board_height - 1
-        snake.current_location[0] = head_location
+        return head_location
+
+    @staticmethod
+    def wrap_around_borders(board, location):
+        board_width = board[0]
+        board_height = board[1]
+        location[0] = 0 if location[0] == board_width else location[0]
+        location[1] = 0 if location[1] == board_height else location[1]
+        location[0] = board_width - 1 if location[0] == -1 else location[0]
+        location[1] = board_height - 1 if location[1] == -1 else location[1]
