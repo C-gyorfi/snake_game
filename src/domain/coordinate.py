5ff43e08a4bb0752364893a5130eca0f306baa60
csabagyorfi <csaba@madetech.com>
2020-10-08 15:37:02 +0100

    Fix linting

--- src/domain/coordinate.py
+++ src/domain/coordinate.py
@@ -1,4 +1,4 @@
 class Coordinate:
     def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+        self.x_coordinate = x
+        self.y_coordinate = y
--- test/domain/test_coordinate.py
+++ test/domain/test_coordinate.py
@@ -4,6 +4,5 @@ from src.domain.coordinate import Coordinate
 
 def test_coordinate_can_have_an_x_and_y_param():
     point = Coordinate(x=3, y=5)
-    assert_that(point.x).is_equal_to(3)
-    assert_that(point.y).is_equal_to(5)
-
+    assert_that(point.x_coordinate).is_equal_to(3)
+    assert_that(point.y_coordinate).is_equal_to(5)
